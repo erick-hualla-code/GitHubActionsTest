5092a26d23eb8fe0b7a9a366f80b2b6a39d92f8f
erick-hualla-code <erick.hualla.code@gmail.com>
2024-03-07 17:42:59 -0500

    Fix(test); Testing change

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,8 @@ class MyApp extends StatelessWidget {
         // state is not lost during the reload. To reset the state, use hot
         // restart instead.
         //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        //This works for code too, not just values: Most code changes can be
+        //tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
